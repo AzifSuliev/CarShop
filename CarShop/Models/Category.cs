353e9414449af1cabc3d1bc390644ebb505fd939
AzifSuliev <azif.suliev.89@list.ru>
2023-12-07 00:22:12 +0600

    Revert "Lessons 41-42"
    This reverts commit 6465234522ac1c85b6913fa2f0502f2eda466919.

--- CarShop/Models/Category.cs
+++ CarShop/Models/Category.cs
@@ -1,5 +1,4 @@
-﻿using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace CarShop.Models
 {
@@ -8,12 +7,7 @@ namespace CarShop.Models
         [Key]
         public int Id { get; set; } // Первичный ключ
         [Required]
-        [DisplayName("Category Name")] // Название для поля ввода 
-        [MaxLength(30)] // Ограничение в 30 символов
         public string? Name { get; set; }
-
-        [DisplayName("Display Order")] // Название для поля ввода 
-        [Range(1,100, ErrorMessage = "Значение вне диапазона!")] // Значения int от 1 до 100
         public int DisplayOrder { get; set; }
     }
 }

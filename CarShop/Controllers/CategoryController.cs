00b3524d86ad0a8e0b379b834356f15f33e5da9c
AzifSuliev <azif.suliev.89@list.ru>
2023-12-12 19:58:41 +0600

    Lessins 44-46

--- CarShop/Controllers/CategoryController.cs
+++ CarShop/Controllers/CategoryController.cs
@@ -19,13 +19,64 @@ namespace CarShop.Controllers
             return View(objCategoryList);
         }
 
-        // Get-method
+
+        // Get-method для редактирования категории
+        /// <summary>
+        /// Этот метод вызывается при нажатии кнопки Edit
+        /// Он определяет, какую именно категорию нужно редактировать
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public IActionResult Edit(int? Id)  
+        {
+            if(Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+            // Метод Find работает только с первичным ключом. Здесь извлекается категория из базы данных
+            Category? categoryFromDb = _db.Categories.Find(Id);
+            // Ещё два способа извлечь категорию из базы данных
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u => u.Id.Equals(Id));
+            //Category? categoryFromDb2 = _db.Categories.Where(u => u.Id == Id).FirstOrDefault();
+            if (categoryFromDb == null)
+            {
+                return NotFound(); 
+            }
+            return View(categoryFromDb);
+        }
+
+        // Post-method для редактирования категории
+        [HttpPost]
+        public IActionResult Edit(Category? obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString()) // Проверка на соответствие полей Name и DisplayOrder
+            {
+                // Ошибка
+                ModelState.AddModelError("name", "The display order cannot exactly match the Name");
+            }
+
+            //if (obj.Name != null && obj.Name.ToLower().Equals("test") || (obj.Name != null && obj.Name.ToLower().Equals("тест")))// Проверка на соответствие полей Name и DisplayOrder
+            //{
+            //    // Ошибка
+            //    ModelState.AddModelError("", "Test (тест) is the invalid value!");
+            //}
+            if (ModelState.IsValid) // Проверка состояния модели
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
+
+
+        // Get-method для создания категории
         public IActionResult Create()
         {
             return View();
         }
 
-        // Post-method
+        // Post-method для создания категории
         [HttpPost]
         public IActionResult Create(Category? obj)
         {

358095f1076d9324f229383bea80774ceca61357
AzifSuliev <azif.suliev.89@list.ru>
2023-12-17 21:02:34 +0600

    Lessons 59-60

---
+++ CarShop_RazorTemp/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,31 @@
+using CarShop_RazorTemp.Data;
+using CarShop_RazorTemp.Models.CarShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CarShop_RazorTemp.Pages.Categories
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public Category Category { get; set; }
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            _db.Categories.Add(Category);
+            _db.SaveChanges();
+            return RedirectToPage("Index");
+        }
+    }
+}

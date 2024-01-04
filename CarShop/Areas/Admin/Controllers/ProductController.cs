54a6087bb451762a989536de992ccf5071612473
AzifSuliev <azif.suliev.89@list.ru>
2024-01-04 22:14:35 +0600

    Lessons 88-89

--- CarShop/Areas/Admin/Controllers/ProductController.cs
+++ CarShop/Areas/Admin/Controllers/ProductController.cs
@@ -1,6 +1,8 @@
 ﻿using CarShop.DataAccess.Repository.IRepository;
 using CarShop.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace CarShop.Areas.Admin.Controllers
 {
@@ -25,6 +27,13 @@ namespace CarShop.Areas.Admin.Controllers
         /// <returns></returns>
         public IActionResult Create() 
         {
+            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.
+                GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                });
+            ViewBag.CategoryList = CategoryList; // Динамический контейнер ViewBag
             return View();
         }
 

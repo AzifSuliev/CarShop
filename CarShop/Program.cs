193346b38400c1cc69404e7464f688e3f070f22c
AzifSuliev <azif.suliev.89@list.ru>
2024-01-27 14:36:26 +0600

    Lesson 119

--- CarShop/Program.cs
+++ CarShop/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false).
-    AddEntityFrameworkStores<ApplicationDbContext>();
+    AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 builder.Services.AddRazorPages();
 // Dependency Injection
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();

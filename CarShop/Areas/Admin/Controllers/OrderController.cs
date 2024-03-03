7a71d97801518d245205fa7bb02fbb497739d453
AzifSuliev <azif.suliev.89@list.ru>
2024-03-03 17:51:02 +0600

    Lessons 157-158

---
+++ CarShop/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,30 @@
+﻿using CarShop.DataAccess.Repository.IRepository;
+using CarShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region APICALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<OrderHeader> objOrderHeaderList = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            return Json(new { data = objOrderHeaderList });
+        }
+        #endregion
+    }
+}
+
---
+++ CarShop/wwwroot/js/order.js
@@ -0,0 +1,33 @@
+﻿var dataTable;
+$(document).ready(function () {
+    loadDataTable();
+});
+
+
+function loadDataTable() {
+    // код из вкладки Ajax сайта DataTables
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/admin/order/getall' }, // путь к методу getall
+        // код из вкладки Data сайта DataTables
+        "columns": [
+            { data: 'id', "width": "10%" },
+            { data: 'name', "width": "40%" },
+            { data: 'phoneNumber', "width": "10%" },
+            { data: 'applicationUser.email', "width": "10%" },
+            { data: 'orderStatus', "width": "10%" },
+            { data: 'orderTotal', "width": "10%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                         <a href="/admin/order/details?orderId=${data}"  class="btn btn-primary" style="height: 40px; width: 160px; border-radius: 10px;"> 
+                         <i class="bi bi-pencil-square"></i></a>
+                    </div>`
+                },              
+                "width": "10%"
+            },
+        ]
+    });
+}
+
+
